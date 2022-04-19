cdb329ea97bf3edb8678c87bad661f49c32eb959
pascal789 <100601866+pascal789@users.noreply.github.com>
2022-04-19 17:54:15 +0900

    Revert "Install enum"

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,3 @@
 Rails.application.routes.draw do
-  # 管理者用
-  devise_for :admins, skip: [:registrations, :passwords], controllers: {
-    sessions: "admin/sessions"
-  }
-  
-  # 顧客用
-  devise_for :customers, skip: [:passwords], controllers: {
-    registrations: "public/registrations",
-    sessions: 'public/sessions'
-  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
